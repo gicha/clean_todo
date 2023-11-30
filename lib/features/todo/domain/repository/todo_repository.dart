18949407a9432fe3886b74ee731c47e715a7fdb1
Mikhail Kolchanov <01gicha01@gmail.com>
2023-11-30 13:37:03 +0800

    describe business cases in abstract todo repo

--- lib/features/todo/domain/repository/todo_repository.dart
+++ lib/features/todo/domain/repository/todo_repository.dart
@@ -1,1 +1,10 @@
-abstract class TodoRepository {}
+import '../entity/task_entity.dart';
+
+abstract class TodoRepository {
+  Future<TaskEntity> getTaskById(int id);
+  Future<List<TaskEntity>> getTasks();
+  Future<TaskEntity> addTask(TaskEntity task);
+  Future<TaskEntity> updateTask(TaskEntity task);
+  Future<void> completeTask(int id);
+  Future<void> revertTask(int id);
+}

a5103b887c51e9d1bcde1a46f158fd9ac0c8cc69
Mikhail Kolchanov <01gicha01@gmail.com>
2023-11-30 16:43:23 +0800

    connect todo dependency

--- lib/features/todo/di/todo_dependencies.dart
+++ lib/features/todo/di/todo_dependencies.dart
@@ -1,10 +1,26 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:todo/features/todo/data/repository/todo_impl.dart';
+import 'package:todo/features/todo/data/source/local/impl/mock_todo_lds.dart';
+import 'package:todo/features/todo/domain/bloc/todo_list/todo_list_bloc.dart';
 
 class TodoDependenciesWidget extends StatelessWidget {
-  const TodoDependenciesWidget({super.key});
+  const TodoDependenciesWidget({
+    super.key,
+    required this.child,
+  });
+
+  final Widget child;
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return BlocProvider(
+      create: (_) {
+        final todoLDS = MockTodoLDS();
+        final todoRepository = TodoRepositoryImpl(todoLDS: todoLDS);
+        return TodoListBloc(todoRepository: todoRepository);
+      },
+      child: child,
+    );
   }
 }
--- lib/features/todo/presentation/todo_widget.dart
+++ lib/features/todo/presentation/todo_widget.dart
@@ -1,10 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:todo/features/todo/di/todo_dependencies.dart';
+import 'package:todo/features/todo/presentation/widgets/todo_list/todo_list_widget.dart';
 
 class TodoWidget extends StatelessWidget {
   const TodoWidget({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return const TodoDependenciesWidget(
+      child: TodoListWidget(),
+    );
   }
 }
